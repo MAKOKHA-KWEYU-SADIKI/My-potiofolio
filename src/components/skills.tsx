435d0a5f584b847be54022cb827842c9601f225a
MAKOKHA-KWEYU-SADIKI <makokha.14460@students.kyu.ac.ke>
2024-10-17 20:08:06 +0300

    updating my skills

--- src/components/skills.tsx
+++ src/components/skills.tsx
@@ -7,7 +7,7 @@ import img5 from '../assets/flex-grid-challange.png'
 function Skills() {
     
         const skill=[
-            {name:"SystemVehicle Hire Management ",img:img1,link:"https://car-hire-frontend-two.vercel.app/", desc:"This is an efficient web-based platform that streamlines the vehicle rental process, allowing users to easily browse available vehicles, make bookings, and manage rentals, offering a seamless and user-friendly experience for both customers and administrators."},
+            {name:"Vehicle Hire Management System",img:img1,link:"https://car-hire-frontend-two.vercel.app/", desc:"This is an efficient web-based platform that streamlines the vehicle rental process, allowing users to easily browse available vehicles, make bookings, and manage rentals, offering a seamless and user-friendly experience for both customers and administrators."},
             {name:"Book repository",img:img3,link:"https://salafy-library-nine.vercel.app/",desc:"The Book Repository is an efficient platform that allows users to search, add, and delete books seamlessly, providing an intuitive and easy-to-use interface for managing a book collection."},
             {name:"To-Do list",img:img2,link:"https://todo-app-pi-eight-53.vercel.app/",desc:"A simple and user-friendly application that allows users to create, manage, and organize their tasks efficiently. "},
             {name:"RestaurantAPI",desc:""},
